ba249f9330b33444b8d673c789b55c8622d78bfe
Roopak Patil <roopakpatil@gmail.com>
2014-03-23 14:31:48 -0700

    Final changes from Rohit's pull and my code

--- docc/recoengine.py
+++ docc/recoengine.py
@@ -81,7 +81,7 @@ class RecoEngine(object):
     def getMostRatedContent(self, sub_category):
         
         try:
-            return self.db['content'].find().sort('Rating', pymongo.DESCENDING)
+            return self.db['content'].find({'sub_category': sub_category}).sort('Rating', pymongo.DESCENDING)
         except:
             traceback.print_exc()
             
